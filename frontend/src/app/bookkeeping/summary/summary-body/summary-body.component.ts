db203081f9084ddb8c49e76ec35e78de2fa1e821
Sergey Koval <skoval@exadel.com>
2017-05-04 19:01:09 +0300

    correct summary styles

--- frontend/src/app/bookkeeping/summary/summary-body/summary-body.component.ts
+++ frontend/src/app/bookkeeping/summary/summary-body/summary-body.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'bk-summary-body',
   templateUrl: './summary-body.component.html',
   styleUrls: ['./summary-body.component.css']
 })
-export class SummaryBodyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class SummaryBodyComponent {
+  @Input()
+  public summaries: Summary[];
+  @Input()
+  public conversionCurrency: Currency;
 }

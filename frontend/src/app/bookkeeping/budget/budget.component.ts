1ee940efb24dea6fec6913a8f2ce886a8648baa3
Sergey Koval <skoval@exadel.com>
2017-08-10 15:04:42 +0300

    BK-3 Transfer tab on history add/edit dialog (test commit)

--- frontend/src/app/bookkeeping/budget/budget.component.ts
+++ frontend/src/app/bookkeeping/budget/budget.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetComponent implements OnInit {
 
-  constructor() { }
+  public items: SelectItem[];
+  public items2: SelectItem[];
+  public selectedItem: SelectItem[];
+  public selectedItem2: SelectItem[];
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.items = [
+      {title: 'Деньги', icon: 'assets/image/other_exp.gif', children: [{title: 'Наличные'}, {title: 'Резерв'}]},
+      {title: 'Сбережения', icon: 'assets/image/other_exp.gif', children: [{title: 'Белинвест', icon: 'assets/image/home.gif'}, {title: 'Альфа', icon: 'assets/image/home.gif'}]},
+      {title: 'Абс', icon: 'assets/image/other_exp.gif', children: [{title: 'Аааа'}, {title: 'Бббб'}]}
+    ];
+
+    this.items2 = [
+      {title: 'Деньги2', icon: 'assets/image/other_exp.gif', children: [{title: 'Наличные2'}, {title: 'Резерв2'}]},
+      {title: 'Сбережения2', icon: 'assets/image/other_exp.gif', children: [{title: 'Белинвест2', icon: 'assets/image/home.gif'}, {title: 'Альфа2', icon: 'assets/image/home.gif'}]},
+      {title: 'Абс2', icon: 'assets/image/other_exp.gif', children: [{title: 'Аааа2'}, {title: 'Бббб2'}]}
+    ];
+
+    this.selectedItem = [{title: 'Сбережения', icon: 'assets/image/other_exp.gif', children: [{title: 'Белинвест', icon: 'assets/image/home.gif'},
+      {title: 'Альфа', icon: 'assets/image/home.gif'}]}, {title: 'Альфа', icon: 'assets/image/home.gif'}];
+
+    this.selectedItem2 = [{title: 'Сбережения2', icon: 'assets/image/other_exp.gif', children: [{title: 'Белинвест2', icon: 'assets/image/home.gif'},
+      {title: 'Альфа2', icon: 'assets/image/home.gif'}]}, {title: 'Альфа2', icon: 'assets/image/home.gif'}];
   }
-
 }

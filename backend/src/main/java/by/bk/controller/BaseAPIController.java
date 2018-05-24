f80353e7ed3d51905fa0b1a8bd8329279a482295
Sergey Koval <skoval@exadel.com>
2018-05-24 15:20:50 +0300

    Fix expired token after expiration check (revert)

--- backend/src/main/java/by/bk/controller/BaseAPIController.java
+++ backend/src/main/java/by/bk/controller/BaseAPIController.java
@@ -1,11 +1,7 @@
 package by.bk.controller;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +12,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public abstract class BaseAPIController {
     protected final Log LOG = LogFactory.getLog(this.getClass());
-
-    @ExceptionHandler({ExpiredJwtException.class})
-    public ResponseEntity<String> handleAuthenticationException(ExpiredJwtException e) {
-        LOG.error(e);
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED.value()).body("EXPIRED");
-    }
 }

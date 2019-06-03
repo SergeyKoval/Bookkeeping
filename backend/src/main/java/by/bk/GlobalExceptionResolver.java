6772d17bd7805de16fa6f136f2114da773d9cb47
Sergey Koval <skoval@exadel.com>
2019-06-03 18:41:40 +0300

    BK-252 Ignore reports for some expected exceptions

--- backend/src/main/java/by/bk/GlobalExceptionResolver.java
+++ backend/src/main/java/by/bk/GlobalExceptionResolver.java
@@ -1,12 +1,14 @@
 package by.bk;
 
 import by.bk.mail.EmailPreparator;
+import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ErrorHandler;
@@ -17,6 +19,7 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author Sergey Koval
@@ -24,9 +27,10 @@ import java.time.LocalDateTime;
 @Component
 public class GlobalExceptionResolver extends SimpleMappingExceptionResolver implements ErrorHandler {
     private static final Log LOG = LogFactory.getLog(GlobalExceptionResolver.class);
-    private static final String REPLACE_PATTERN = "\\r\\n\\t";
+    private static final String REPLACE_PATTERN = System.getProperty("line.separator");
     private static final String REPLACEMENT = "<br>&emsp;";
     private static final String SYSTEM_USER = "SYSTEM";
+    private static final List<Class> EXPECTED_EXCEPTIONS = List.of(BadCredentialsException.class);
 
     @Value("${mail.admin.username}")
     private String toEmail;
@@ -42,11 +46,12 @@ public class GlobalExceptionResolver extends SimpleMappingExceptionResolver impl
 
     @Override
     protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-//        String stackTrace = RegExUtils.replaceAll(ExceptionUtils.getStackTrace(ex), REPLACE_PATTERN, REPLACEMENT);
-        String stackTrace = ExceptionUtils.getStackTrace(ex);
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (notify) {
-            exceptionNotificationEmailPreparator.prepareAndSend(toEmail, LocalDateTime.now(), principal, stackTrace);
+        if (!EXPECTED_EXCEPTIONS.contains(ex.getClass())) {
+            String stackTrace = RegExUtils.replaceAll(ExceptionUtils.getStackTrace(ex), REPLACE_PATTERN, REPLACEMENT);
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (notify) {
+                exceptionNotificationEmailPreparator.prepareAndSend(toEmail, LocalDateTime.now(), principal, stackTrace);
+            }
         }
         return super.doResolveException(request, response, handler, ex);
     }
@@ -54,8 +59,7 @@ public class GlobalExceptionResolver extends SimpleMappingExceptionResolver impl
     @Override
     public void handleError(Throwable t) {
         LOG.error(t.getMessage(), t);
-//        String stackTrace = RegExUtils.replaceAll(ExceptionUtils.getStackTrace(t), REPLACE_PATTERN, REPLACEMENT);
-        String stackTrace = ExceptionUtils.getStackTrace(t);
+        String stackTrace = RegExUtils.replaceAll(ExceptionUtils.getStackTrace(t), REPLACE_PATTERN, REPLACEMENT);
         if (notify) {
             exceptionNotificationEmailPreparator.prepareAndSend(toEmail, LocalDateTime.now(), SYSTEM_USER, stackTrace);
         }

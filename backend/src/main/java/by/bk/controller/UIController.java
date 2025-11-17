5849948144c3232f8f60367fea3ed81f04a7eb80
sergey.koval <sergey.koval@instructure.com>
2025-11-17 20:04:40 +0300

    Separate backend-frontend logic

--- backend/src/main/java/by/bk/controller/UIController.java
+++ backend/src/main/java/by/bk/controller/UIController.java
@@ -6,7 +6,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -14,18 +13,12 @@ import java.io.IOException;
 /**
  * @author Sergey Koval
  */
-@Controller
+@RestController
 public class UIController {
     @Value("classpath:mobile-app/android/bookkeeper-1.3.11.apk")
     private Resource androidApplication;
 
-    @RequestMapping(value = "/**/{path:[^\\.]+}", method = RequestMethod.GET)
-    public String redirect() {
-        return "forward:/";
-    }
-
     @RequestMapping(value = "/mobile-app/android", method = RequestMethod.GET)
-    @ResponseBody
     public ResponseEntity<byte[]> getAndroidMobileApplication() throws IOException {
         return ResponseEntity.ok()
                         .contentType(MediaType.parseMediaType("application/vnd.android.package-archive"))
